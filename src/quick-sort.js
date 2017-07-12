936ccce4c2809470f14545bf952abac14385a45f
Srinivas Pogula <Srinivas_Pogula@epam.com>
2017-07-12 13:37:27 +0530

    quick sort implementation

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -1,6 +1,35 @@
 'use strict';
 
 function quickSort(input) {
+	
+	//var input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 99, 100];
+	//system.out.println(input);
+	//if (Array.isArray)
+    //return Array.isArray(input);
+	//end;	
+	if (input instanceof Array)
+	{
+    println ('value is array');
+	} else
+	{
+	println('not an array');
+    }
 
-};
+	
+	if(input.length > 0){   
+    println ('array is not empty'); 
+	
+    }else 
+	{
+     println ('array is not empty'); 
+}
+    //if (input[index]=="")
+	//{
+    // alert ('empty');
+	//} else
+	//alert ('not empty');
+	
+	//input = [1,3,7,5];
+    //input instanceof Array;
+	};
 module.exports = quickSort;
